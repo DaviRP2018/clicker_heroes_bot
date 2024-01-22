c77cb8446e285b0c7211e4d8613e3378e0bc33e4
Davi Rodriguez <davi.rodriguez@jala.university>
2024-01-21 22:23:42 -0300

    Minor one_repo update

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 import json
 import time
+
 from ctypes import windll
 
 import keyboard
@@ -10,32 +11,32 @@ from settings.settings import NUMBER_OF_AUTO_CLICKERS
 
 
 def manual_calibrate_colors():
-    """ Calibrate colors """
-    print('Press Ctrl-C to quit.')
+    """Calibrate colors"""
+    print("Press Ctrl-C to quit.")
     dc = windll.user32.GetDC(0)
     try:
         while True:
             x, y = pyautogui.position()
             rgb = windll.gdi32.GetPixel(dc, x, y)
-            r = rgb & 0xff
-            g = (rgb >> 8) & 0xff
-            b = (rgb >> 16) & 0xff
+            r = rgb & 0xFF
+            g = (rgb >> 8) & 0xFF
+            b = (rgb >> 16) & 0xFF
             color = "{} {} {}".format(r, g, b)
-            print(color, end='')
-            print('\b' * len(color), end='', flush=True)
+            print(color, end="")
+            print("\b" * len(color), end="", flush=True)
     except KeyboardInterrupt:
         return
 
 
 def manual_calibrate_positions():
-    """ Calibrate positions """
-    print('Press Ctrl-C to quit.')
+    """Calibrate positions"""
+    print("Press Ctrl-C to quit.")
     try:
         while True:
             x, y = pyautogui.position()
-            position_str = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-            print(position_str, end='')
-            print('\b' * len(position_str), end='', flush=True)
+            position_str = "X: " + str(x).rjust(4) + " Y: " + str(y).rjust(4)
+            print(position_str, end="")
+            print("\b" * len(position_str), end="", flush=True)
     except KeyboardInterrupt:
         return
 
@@ -64,7 +65,10 @@ def calibrate_colors():
                     b = (rgb >> 16) & 0xFF
                     colors["hero_buy_all_upgrades"] = (r, g, b)
                     progress += 1
-                    print("hero_buy_all_upgrades", colors["hero_buy_all_upgrades"])
+                    print(
+                        "hero_buy_all_upgrades",
+                        colors["hero_buy_all_upgrades"],
+                    )
                     time.sleep(1)
         except KeyboardInterrupt:
             pass
@@ -174,7 +178,7 @@ def scroll_hero_down():
 def get_color(x, y):
     """Return a list containing the RGB of a given position"""
     dc = windll.user32.GetDC(0)
-    rgb = windll.gdi32.GetPixel(dc, x, y)
+    rgb = windll.gdi32.GetPixel(dc, int(x), int(y))
     r = rgb & 0xFF
     g = (rgb >> 8) & 0xFF
     b = (rgb >> 16) & 0xFF
@@ -192,11 +196,11 @@ def upgrade_all():
         with open("settings/colors.json") as json_file:
             colors = json.load(json_file)
             if (
-                    get_color(
-                        positions["hero_buy_all_upgrades"][0],
-                        positions["hero_buy_all_upgrades"][1],
-                    )
-                    == colors["hero_buy_all_upgrades"]
+                get_color(
+                    positions["hero_buy_all_upgrades"][0],
+                    positions["hero_buy_all_upgrades"][1],
+                )
+                == colors["hero_buy_all_upgrades"]
             ):
                 pyautogui.click(
                     positions["hero_buy_all_upgrades"][0],
@@ -241,12 +245,12 @@ def hire_all_relevant_heroes():
     scroll_hero_up_maximum()
     time.sleep(1 / 2)
     # change to hire MAX
-    for i in range(0, 4):
+    for _ in range(0, 4):
         pyautogui.press("t")
         time.sleep(1 / 2)
     for item in relevant_heroes_pos_with_scrolls:
         if isinstance(item, int):
-            for i in range(0, item):
+            for _ in range(0, item):
                 scroll_hero_down()
                 time.sleep(1 / 2)
         else:
@@ -260,7 +264,9 @@ def reset_auto_clickers():
     time.sleep(1 / 2)
     with open("settings/positions.json") as json_file:
         positions = json.load(json_file)
-        pyautogui.click(positions["auto_clicker"][0], positions["auto_clicker"][1])
+        pyautogui.click(
+            positions["auto_clicker"][0], positions["auto_clicker"][1]
+        )
     time.sleep(1 / 2)
     pyautogui.keyDown("c")
 
@@ -269,8 +275,10 @@ def set_auto_clickers_to_damage():
     pyautogui.keyDown("c")
     with open("settings/positions.json") as json_file:
         positions = json.load(json_file)
-        for i in range(1, NUMBER_OF_AUTO_CLICKERS):
-            pyautogui.click(positions["gold_pickup"][2], positions["gold_pickup"][0])
+        for _ in range(1, NUMBER_OF_AUTO_CLICKERS):
+            pyautogui.click(
+                positions["gold_pickup"][2], positions["gold_pickup"][0]
+            )
             time.sleep(1)
     pyautogui.keyUp("c")
 
